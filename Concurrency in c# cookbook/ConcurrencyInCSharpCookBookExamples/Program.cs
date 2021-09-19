340e5126aaaee4065fe3391e5cb598d5364ce0d2
Ahmed Shaker <Ahmed.Shaker@integrant.com>
2021-09-19 14:50:23 +0200

    dd

---
+++ Concurrency in c# cookbook/ConcurrencyInCSharpCookBookExamples/Program.cs
@@ -0,0 +1,34 @@
+﻿using Nito.AsyncEx;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConcurrencyInCSharpCookBookExamples
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var tasks1 = Task.Run(() => DelayAndReturnAsync(300));
+            var tasks2 = Task.Run(() => DelayAndReturnAsync(100));
+            var tasks3 = Task.Run(() => DelayAndReturnAsync(200));
+
+            Task<int>[] tasks = new Task<int>[] { tasks1, tasks2, tasks3 };
+            var processingTasks = tasks.Select(async t =>
+            {
+                var result = await t;
+                Console.WriteLine(result);
+            }).ToArray();
+
+            await Task.WhenAll(processingTasks);
+            Console.ReadLine();
+        }
+        static async Task<int> DelayAndReturnAsync(int value)
+        {
+            await Task.Delay(value);
+           // Console.WriteLine(value);
+            return value;
+        }
+    }
+}
